37c878ab874e0362acc11c9a853d2015cce187fa
LoZzR <zakariaelhichem@gmail.com>
2022-01-23 13:53:46 +0100

    Add person controller first impl

---
+++ src/main/java/controllers/PersonController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import services.IPersonService;
+
+@Controller
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private IPersonService personService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String getAllPersons(Model model){
+        model.addAttribute("persons", personService.findAll());
+        return "persons";
+    }
+}
